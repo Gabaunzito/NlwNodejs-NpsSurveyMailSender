0c43f22b59eed88900ddcf6fe59bbf7abf7650c3
Gabaunzito <gabrielpmachado.contato@gmail.com>
2021-02-25 22:00:43 -0300

    Added Email sender service

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { SurveysController } from './controllers/SurveysControllers';
+import { SendMailController } from './controllers/SendMailController';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from "./controllers/UserController"
 
 const router = Router();
@@ -7,11 +8,14 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute)
 
 
 export { router };
